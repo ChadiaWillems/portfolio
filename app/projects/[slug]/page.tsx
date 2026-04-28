31d5a56f6ed07b6c9c5b0ac2edb0e49bb52dfea7
ChadiaWillems <chadia.willems@student.howest.be>
2026-04-28 19:31:59 +0200

    Remove unused dynamicParams export from ProjectDetail component

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -5,7 +5,6 @@ import { unstable_cache } from 'next/cache';
 import Nav from '@/app/components/nav/Nav';
 import Footer from '@/app/components/nav/Footer';
 
-export const dynamicParams = true;
 
 export async function generateStaticParams() {
   const projects = await client.fetch(`*[_type == "project"]{ "slug": slug.current }`);
